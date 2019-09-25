988cddfc3d6709180a9a5fdb9165950477ba880d
hwlcode <hwlcode@163.com>
2019-09-25 18:59:30 +0800

    update

---
+++ src/pages/customer-service/customer-service.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="customer-service">
+
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+        name: "customer-service"
+    };
+</script>
+
+<style lang="less" scoped>
+    .customer-service {
+
+    }
+</style>
---
+++ src/pages/privacy/privacy.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="privacy">
+
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+        name: "privacy"
+    };
+</script>
+
+<style lang="less" scoped>
+    .privacy {
+
+    }
+</style>
